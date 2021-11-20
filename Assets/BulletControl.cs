708ce9d8dad434d813232b4a484374fcc5c3e280
ChristopherRDunn <crdunn@shaw.ca>
2021-11-20 11:28:25 -0800

    Add walls and bullet collision

--- Assets/BulletControl.cs
+++ Assets/BulletControl.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class BulletControl : MonoBehaviour
 {
+    private Rigidbody2D rb2d;
+    private int wallCollisions;
+    public int maxWallCollisions = 3;
+
     // Start is called before the first frame update
     void Start()
     {
+        rb2d = GetComponent<Rigidbody2D>();
+        wallCollisions = 0;
         
+        // Uncomment to test bullet collision
+        // testMove();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void testMove() {
+        // This starts the bullet moving, use to test it out
+        var velocity = rb2d.velocity;
+        velocity.y = 5;
+        rb2d.velocity = velocity;
+    }
+
+    void OnCollisionEnter2D (Collision2D coll) {
+        if (coll.collider.CompareTag("Player")) {
+           // TODO: Kill the bullet, kill the player, set in motion the score increase and respawn
+        } else if (coll.collider.CompareTag("Wall")) {
+            wallCollisions++;
+            if (wallCollisions > maxWallCollisions) {
+                wallCollisions = 0;
+                rb2d.velocity = new Vector2(0, 0);
+                transform.position = new Vector2(0, 0);
+                // TODO: Kill the bullet
+            }
+        }
     }
 }
